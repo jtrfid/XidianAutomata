4df88c07c951695a2b0657b3508feccfa2a23d79
jtrfid <jtrfid@qq.com>
2018-10-15 00:54:47 +0800

    StateTo<StateSet> Test

--- Automata/StateTo.h
+++ Automata/StateTo.h
@@ -32,8 +32,24 @@
 #include <cassert>  // 必须包含
 #include "State.h"
 
+/*************************************************************************************
 // Map State's to class T. Used to implement transitions(TransRel and DTransRel)
 // and State relations(StateRel and StateEqRel).
+
+   StateTo<T> 描述state to T(eg. StateSet)的映射(map)关系。
+   T *data; 内部使用data[state]表示这种映射关系。
+   set_domain(const int r)设置动态数组data的大小，表示存储这种关系的容量,对*data分配了存储空间。
+   int howmany; 表示动态数组data大小，包含扩充部分(expansion_extra常量)
+   int in_use; 表示实际使用的data大小。
+
+   以下两个函数都是返回state r to T的映射(map),前者返回Not const T&，用于设置；后者返回const T&，用于查询。
+   二者均没有给data[r]赋值。留给继承类完成。
+   T& map(const State r); retun data[r];  Not const，用于设置；
+   const T& lookup(const State r) return data[r]，用于查询。在派生类中给data[r]赋值后才能使用。
+
+   StateTo<T>& StateTo<T>::disjointing_union(const StateTo<T>& r)
+  合并this和r ==> this，保证不重叠, this.domain += r.domain;
+**************************************************************************************/
 template<class T>
 class StateTo
 {
